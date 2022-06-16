43291f24d0bc1f419e211a673bb35b51b7563551
DINESH MADAAN <riamadan2014@gmail.com>
2022-06-16 18:02:38 +0530

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+import image from "./media/bg2.jpg" 
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+    // <br/>      <br/> 
+   <StyledApp>
+      <header>
+        <img src={image}  />
+    </header> 
+
+    <form>
+      <div>
+        <label>
+          
+          <input type="text" placeholder="Your Team Name"></input>
+         </label>
+        
+       
+        <label>
+             
+          <input type="text" placeholder="Your Name"></input>         
+        </label>
+        
+        
+        <label>
+         
+          <input type="password" placeholder="Create Password" ></input>
+        </label>
+        
+        <div>
+
+       <button>
+        Next
+        </button>
+        </div>
+      </div>
+    </form>
+       </StyledApp>
+      
+    
+  )
 }
 
+const StyledApp = styled.div`
+  width: 100%;
+  height: 100vh; 
+  background:grey;
+  position: relative;
+    
+  header {
+    width: 100%;
+    height: 100%; 
+    margin:0px;
+    overflow:hidden;
+    background:grey;
+  }
+  img{
+    width:100%;
+    height:100%;
+
+    margin=0px;
+   
+  }
+  form {
+    position: absolute;
+    top: 25%;
+    left: 55%;
+    display:flex;
+    height:40%;
+    width:17%;
+    
+    
+    
+
+  }
+  
+  label{
+    display:block;    
+    background: rgba(0 0 0 0);
+    margin:10px;
+    
+    
+    
+   
+    
+
+  }
+  input, text-area{
+
+   
+    width: 100%;
+    background:transparent;
+    border:none;
+    border-bottom:solid black;
+    padding:3px;
+    
+
+    
+    
+    
+  
+    
+  }
+  button{
+  background:orange;
+  position:relative;
+  left:70%;
+  
+ 
+  
+  border-radius: 25px;
+  border:none;
+  padding:3px;
+  max-width:50px;
+  width:100%;
+  margin:7px;
+
+  
+    }
+
+
+`
+
+
+
+
 export default App;
+
+/*Comment
+form
+
+
+*/
